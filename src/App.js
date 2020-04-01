dc7773e40657ab537198e4e17024b9b0bf9078b8
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-04-01 11:18:18 -0600

    create dates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // Arreglo de citas
+const [citas, setSaveCitas] = useState([]);
+
+// Funcion tome citas actuales y agregue nueva cita
+const crearCita = cita => {
+  setSaveCitas([ ...citas, cita ])
+}
+
   // Date actually Footer
   const date = new Date().getFullYear();
 
@@ -18,7 +27,9 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="one-half column">
-              <Formulario />
+              <Formulario 
+              crearCita={crearCita}
+              />
           </div>
           <div className="one-half column">
               2
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,17 +1,79 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+const { v4: uuidv4 } = require('uuid');
 
-const Formulario = () => {
+
+const Formulario = (props) => {
+
+    // Crear State de citas
+    const [cita, setCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const [error, setError] = useState(false)
+
+    // Función que se actualiza cuando el usuario escribe en un input
+    const actualizarState = e => {
+        setCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Extraer valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita
+
+    // Cuando el usuario haga click en cita
+    const submitCita = e => {
+       e.preventDefault();
+       
+       // Validar
+       if (mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || 
+       hora.trim() === '' || sintomas.trim() === '') {  
+           setError(true);
+           return;
+       }
+
+       // Eliminar mensaje previo de error
+       setError(false);
+
+       // Asignar ID
+       cita.id = uuidv4();
+       
+       // Crear cita
+       props.crearCita(cita)
+
+       // Reiniciar form
+       setCita({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+       })
+    }
+
     return ( 
     <Fragment>
         <h2>Crear Cita</h2>
 
-        <form>
+        {error ? <p className="alerta-error">Todos los campos son obligatorios</p>
+        : null}
+
+        <form
+        onSubmit={submitCita}
+        >
             <label>Nombre Mascota</label>
             <input
                 type="text"
                 name="mascota"
                 className="u-full-width"
                 placeholder="Nombre Mascota"
+                onChange={actualizarState}
+                value={mascota}
             />
 
             <label>Nombre Dueño</label>
@@ -20,6 +82,8 @@ const Formulario = () => {
                 name="propietario"
                 className="u-full-width"
                 placeholder="Nombre Dueño de la mascota"
+                onChange={actualizarState}
+                value={propietario}
             />
             
             <label>Fecha de alta</label>
@@ -27,6 +91,8 @@ const Formulario = () => {
                 type="date"
                 name="fecha"
                 className="u-full-width"
+                onChange={actualizarState}
+                value={fecha}
             />
             
             <label>Hora</label>
@@ -34,12 +100,16 @@ const Formulario = () => {
                 type="time"
                 name="hora"
                 className="u-full-width"
+                onChange={actualizarState}
+                value={hora}
             />
 
             <label>Síntomas</label>
             <textarea
             className="u-full-width"
             name="sintomas"
+            onChange={actualizarState}
+            value={sintomas}
             ></textarea>
             
             <button
